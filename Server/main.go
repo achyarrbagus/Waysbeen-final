9412b8487206fce48c773a6a07b796b606a3d05e
achyarrbagus <achyarbagus@gmail.com>
2023-03-15 13:33:13 +0700

    initial-commit

--- Server/main.go
+++ Server/main.go
@@ -35,5 +35,5 @@ func main() {
 
 	e.Static("/uploads", "./uploads")
 	fmt.Println("server running localhost:" + port)
-	e.Logger.Fatal(e.Start("localhost:" + port)) // delete localhost
+	e.Logger.Fatal(e.Start(":" + port)) // delete localhost
 }
